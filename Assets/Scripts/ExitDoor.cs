d1ac25768d09862ee2dcb801c40e1ab9ed455e93
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2023-02-14 13:20:31 +0000

    Exit door script began

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,29 @@
+﻿# region 'Using' information
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class ExitDoor : MonoBehaviour
+{
+    private AudioSource finishSound;
+
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            finishSound.Play();
+            CompleteLevel();
+        }
+    }
+
+    private void CompleteLevel()
+    {
+
+    }
+}
